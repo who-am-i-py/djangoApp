4abf17501a5b5c75646ba56c2fb3d4797f20c755
who-am-i-py <sys.whoami.py@gmail.com>
2023-10-16 20:09:22 +0100

    Modified names of properties

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -6,8 +6,8 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
-    created = models.DateTimeField(default=timezone.now)
-    published = models.DateTimeField(auto_now_add=True)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     def __str__(self) :
         return f"[{self.title}]"
