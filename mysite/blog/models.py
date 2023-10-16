aec2440996549c655453297229b6acb5b62e2d6a
who-am-i-py <sys.whoami.py@gmail.com>
2023-10-16 21:16:23 +0100

    Create Status subclass for choices and add new propertie to Post class model , set default value to DRAFT

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,12 +3,17 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
+    # Status subclass:
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB','Published'
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default= Status.DRAFT)
     # Defining a default sort order:
     class Meta:
         # ordering with reverse chonological order by adding -
