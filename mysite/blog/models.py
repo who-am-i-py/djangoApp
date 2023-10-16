71ec62e060d9f6ad918a4732545f7b7d6a74716a
who-am-i-py <sys.whoami.py@gmail.com>
2023-10-16 19:28:24 +0100

    Add time fields for class model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,9 +1,13 @@
 from django.db import models
-
+# Importing necessary modules :
+from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+    published = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     def __str__(self) :
         return f"[{self.title}]"
