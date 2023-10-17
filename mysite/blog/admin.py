d9b6c550ae2f67310a30d270dea4bd3997294b11
who-am-i-py <sys.whoami.py@gmail.com>
2023-10-17 10:07:23 +0100

    Here we customized the admin model

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
-admin.site.register(Post)
+# Customizing models are displayed in admin site interface
+# admin.site.register(Post)
+# Here the decorator  
+# We are telling the Django Administration site the the model is regitered in the site
+# using a custom class that inherits from ModelAdmin
+# and we include in our custom class  information about how to display model on the site
+# and how interat wit it 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # list_display: an attribute allows us to set the fields of our model that we want 
+    # to display in administration object page.
+    list_display = ["title","slug","author","publish","status"]
+    # Here more options:
+    list_filter = ["status","created","publish","author"]
+    search_fields = ['title','body']
+    raw_id_fields = ['author']
+    date_hierarchy = "publish"
+    ordering = ["status","publish"]
+    prepopulated_fields = {'slug':('title',)}
