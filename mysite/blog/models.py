b7931c647f19a7d5ab29b415de124fd64e870b83
who-am-i-py <sys.whoami.py@gmail.com>
2023-10-17 07:05:02 +0100

    Import User models and create many to one relationship

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 # Importing necessary modules :
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     # Status subclass:
@@ -10,6 +11,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
