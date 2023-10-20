e02e55a825765d9fbd69ad38342abf7626405286
who-am-i-py <sys.whoami.py@gmail.com>
2023-10-20 21:03:22 +0200

    Create detail view for post

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.http import Http404
 from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.published.all()
     return render(request,'blog/post/list.html',{'posts':posts})
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id = id) 
+    except Post.DoesNotExist:
+        raise Http404("Item not found!!")
+    return render(request,'blog/post/detail.html',{'post':post})
