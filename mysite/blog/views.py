c9e06bba7674dcd08f3b7727588ea9399fddb48c
who-am-i-py <sys.whoami.py@gmail.com>
2023-10-26 17:54:31 +0200

    Here we switch between two view function

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,14 +6,14 @@ def post_list(request):
     posts = Post.published.all()
     return render(request,'blog/post/list.html',{'posts':posts})
 
-#def post_detail(request, id):
-#    try:
-#       post = Post.published.get(id = id)
-#    except Post.DoesNotExist:
-#        raise Http404("Item not found!!")
-#   return render(request,'blog/post/detail.html',{'post':post})
-
-
 def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/post_detail.html', {'post':post})
+   try:
+      post = Post.published.get(id = id)
+   except Post.DoesNotExist:
+       raise Http404("Item not found!!")
+   return render(request,'blog/post/detail.html',{'post':post})
+
+
+# def post_detail(request, id):
+#     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+#     return render(request, 'blog/post/post_detail.html', {'post':post})
